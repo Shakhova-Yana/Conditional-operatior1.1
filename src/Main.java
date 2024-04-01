f6d1592ca0d67d217544491785a2fbc270fb8a7b
НафНафДурикович <yana88yana99@gmail.com>
2024-04-01 22:25:40 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nTask 1");
+        int age = 25;
+        if (age >= 18) {System.out.println("ты совершеннолетний");
+        } else {
+            System.out.println("ты не достиг совершеннолетия, нужно немного подождать)");
+        }
+    }
+}
