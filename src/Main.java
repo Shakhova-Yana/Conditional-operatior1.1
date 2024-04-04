75afa8978a6aa55472d1fb8ed42f946e4ae14f66
НафНафДурикович <yana88yana99@gmail.com>
2024-04-04 21:29:02 +0300

    Задание 2. Исправлено 1 задание.

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,13 @@ public class Main {
         } else {
             System.out.printf("Если возраст человека %s равен,то ты не достиг совершеннолетия, нужно немного подождать.\n"
                     , age);}
+        //option 2
         int age2 = 13;
         if (age2 >= 18) {System.out.printf("Если возраст человека %s равен,то ты совершеннолетний.\n", age);
         } else {
             System.out.printf("Если возраст человека %s равен,то ты не достиг совершеннолетия, нужно немного подождать.\n"
                     , age2);}
-
+        //Task 2
         System.out.println("\nTask 2");
         int outsideTemperature = 17;
         if (outsideTemperature <= 5) {
